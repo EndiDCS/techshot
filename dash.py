e792300373a9c7c042e5b209f35f3578de2e4379
Endi <endicoelho@gmail.com>
2022-11-24 21:55:12 -0300

    add movies

--- dash.py
+++ dash.py
@@ -9,34 +9,70 @@ nltk.download('omw-1.4')
 nltk.download('punkt')
 from main import *
 
+
+
+
 st.set_page_config(
     page_title="Avaliando Filmes",
     page_icon=":)"
 )
 
-st.markdown("# Avaliando filmes ")
+titulos = [
+    "### 1: Orgulho e preconceito",
+    "### 2: O jogo da imitação",
+    "### 3: Whisper of the Heart"
+]
+
+imagens = [
+    "orgulho.jpg",
+    "imitacao.jpg",
+    "whisper.jpg"
+]
+st.markdown("# Avaliando filmes: Avalie os filmes abaixo ")
+for i in range(0,len(titulos)):
 
 
 
-review = st.text_input('Review', 'I love this movie the drama and all his story is so good.')
-st.write('The current movie title is', review)
+    st.markdown(titulos[i])
 
-review = aplica_transformacoes([review])
+    col1, col2 = st.columns(2)
+
+    with col1:
+        image = Image.open(imagens[i])
+        st.image(image, caption=None, clamp=False, channels="RGB", output_format="auto")
+    with col2:
+        review = st.text_area('Review', '',key=i)
+        if len(review) <= 1 :
+            hide=True
+        else:
+            hide=False
+
+        review = aplica_transformacoes([review])
+
+        d = {
+            'review': review
+        }
+
+        with open("model.pickle", "rb") as input_file:
+            model = pickle.load(input_file)
+
+        resultado = model.predict(review)
+
+        if hide==False:
+            if resultado == 'positive':
+                st.success("Review positivo")
+            else:
+                st.error("Review negativo")
 
 
 
 
-d = {
-    'review':review
-}
 
-with open("model.pickle","rb") as input_file:
-    model = pickle.load(input_file)
 
-resultado = model.predict(review)
 
-if resultado == 'positive':
-    st.success("Review positivo")
-else:
-    st.error("Review negativo")
-st.write(model)
+
+
+
+
+
+
